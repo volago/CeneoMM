ff6b45825d39f5e894ab89b0b53a4bbc00f602ad
Szymon Szylhabel <szymon.szylhabel@gmail.com>
2023-10-03 14:18:10 +0200

    Task6 commandbus

---
+++ Task6 - CommandBus/SFC - CommandBus.Start/src/SFC.Notifications/AutofacNotificationsModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using SFC.Infrastructure;
+using SFC.Infrastructure.Interfaces;
+using SFC.Notifications.Features.SendNotification;
+using SFC.Notifications.Infrastructure;
+
+namespace SFC.Notifications
+{
+  public class AutofacNotificationsModule : Module
+  {
+    private readonly string _connectionString;
+
+    public AutofacNotificationsModule()
+    {      
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<EmailRepository>()
+        .AsImplementedInterfaces();
+
+      builder.RegisterType<NotificationRepository>()
+        .AsImplementedInterfaces();
+
+      builder.RegisterAssemblyTypes(GetType().Assembly)
+        .AsClosedTypesOf(typeof(ICommandHandler<>)).AsImplementedInterfaces()
+        .InstancePerLifetimeScope();
+
+      builder.RegisterAssemblyTypes(GetType().Assembly)
+        .AsClosedTypesOf(typeof(IEventHandler<>)).AsImplementedInterfaces()
+        .InstancePerLifetimeScope();
+    }
+  }
+}
---
+++ Task6 - CommandBus/SFC - CommandBus.Start/src/SFC.Notifications/Features/NotificationQuery/INotificationPerspective.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFC.SharedKernel;
+
+namespace SFC.Notifications.Features.NotificationQuery
+{
+    public interface INotificationPerspective
+    {
+        IEnumerable<NotificationsCountResult> GetSendNotificationsCount(
+          string notificationType,
+          params LoginName[] loginNames);
+
+    }
+}
