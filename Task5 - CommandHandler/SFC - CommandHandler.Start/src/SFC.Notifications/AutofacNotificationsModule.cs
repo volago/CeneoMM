1112846aa05cbc7bc74325fad75e9bd33f66a3e7
Szymon Szylhabel <szymon.szylhabel@gmail.com>
2023-10-03 12:06:00 +0200

    command handler

---
+++ Task5 - CommandHandler/SFC - CommandHandler.Start/src/SFC.Notifications/AutofacNotificationsModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+namespace SFC.Notifications
+{
+
+  public class AutofacNotificationsModule : Module
+  {
+
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<EmailRepository>()
+        .AsImplementedInterfaces();
+
+      builder.RegisterType<NotificationRepository>()
+        .AsImplementedInterfaces();
+
+      builder.RegisterType<NotificationService>()
+        .AsImplementedInterfaces();
+
+    }
+  }
+}
---
+++ Task5 - CommandHandler/SFC - CommandHandler.Start/src/SFC.Tests/Mocks/TestSmtpClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using SFC.Infrastructure.Interfaces;
+using SFC.Notifications;
+using SFC.SharedKernel;
+
+namespace SFC.Tests.UseStories.Mocks
+{
+  public class TestSmtpClient : ISmtpClient
+  {
+    public static List<SmtpClientEmail> SentEmails { get; set; } = new List<SmtpClientEmail>();
+    public void Send(Email email, string title, string body)
+    {
+      SentEmails.Add(new SmtpClientEmail(email, title, body));
+    }
+
+    public static void Clear()
+    {
+      SentEmails.Clear();
+    }
+  }
+}
